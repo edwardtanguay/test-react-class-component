63986d2bcb38f7b6e8886d628959b9c7fe804390
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-10 14:49:16 +0100

    finished

--- src/components/RecordingManager.js
+++ src/components/RecordingManager.js
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 
-export const RecordingManager = ({audioManager}) => {
+export const RecordingManager = ({ audioManager }) => {
 	const [isRecording, setIsRecording] = useState(false);
 	const [recordingArray, setRecordingArray] = useState(['F', 'E']);
-
 
 	const handleRecordButton = () => {
 		setIsRecording(!isRecording);
 	}
 
 	const recordNote = (note) => {
-		recordingArray.push(note);
-		setRecordingArray([...recordingArray]);
+		if (isRecording) {
+			recordingArray.push(note);
+			setRecordingArray([...recordingArray]);
+		}
 	}
 
 	audioManager.injectRecordNote(recordNote);
@@ -22,7 +23,7 @@ export const RecordingManager = ({audioManager}) => {
 			<button onClick={handleRecordButton}>Record</button>
 			<div>{isRecording ? 'recording' : 'off'}</div>
 			<div><button onClick={() => recordNote('C#')}>Record Note</button></div>
-			{recordingArray.map((note,i) => {
+			{recordingArray.map((note, i) => {
 				return (
 					<div key={i}>{note}</div>
 				)
